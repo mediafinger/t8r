29e69604669ae22401bf9b3c5fc410e8ac6d309e
Andreas Finger <webmaster@mediafinger.com>
2014-03-01 05:33:59 +0100

    Create or invalidate translations when phrase changes

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -4,4 +4,20 @@ class Phrase < ActiveRecord::Base
 
   validates :app_id, presence: true
   validates :key,    presence: true,  uniqueness: { scope: :app }
+
+  after_create :create_translations
+  after_update :update_translations
+
+  def create_translations
+    app.locales.each do |locale|
+      Translation.create!(app_id: app.id, locale_id: locale.id, phrase_id: self.id)
+    end
+  end
+
+  def update_translations
+    app.locales.each do |locale|
+      t = Translation.where(app_id: app.id, locale_id: locale.id, phrase_id: self.id).first
+      t.update_attributes!(done: false) if t.done
+    end
+  end
 end
