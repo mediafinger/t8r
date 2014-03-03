c38cedf34132aa148a51fa5278d99af5f588382f
Andreas Finger <andreas.finger@xing.com>
2014-03-03 20:12:32 +0100

    Allow in-place editing of translations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require best_in_place
 //= require turbolinks
 //= require_tree .
+
+
+// TODO: move to appropriate place:
+
+$(document).ready(function() {
+  /* Activating Best In Place */
+  jQuery(".best_in_place").best_in_place();
+});
