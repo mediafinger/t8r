424004eec5ad288be359148f773f934c3832ebf5
Andreas Finger <webmaster@mediafinger.com>
2014-03-09 23:11:01 +0100

    Update comments

--- db/migrate/20140307063008_change_fields_to_case_insensitive_citext.rb
+++ db/migrate/20140307063008_change_fields_to_case_insensitive_citext.rb
@@ -14,6 +14,15 @@ class ChangeFieldsToCaseInsensitiveCitext < ActiveRecord::Migration
   # then run migration
   # http://www.sfcgeorge.co.uk/posts/2013/11/12/case-insensitive-usernames-with-postgres
 
+  # In case Travis-CI is used, add to travis.yml:
+
+  # before_script:
+  # - psql -c 'create database t8r_test;' -U postgres
+  # - psql -c 'create extension citext;' -U postgres -d t8r_test
+  # - "bundle exec rake db:migrate"
+  # - "bundle exec rake db:test:prepare"
+
+
     def up
       change_table :apps, :bulk => true do |t|
         t.change :name, :citext
